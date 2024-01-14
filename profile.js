a51a18ba66f642a3e67148362959655ecef4b69a
Linda Tran <133718039+lindatranlt@users.noreply.github.com>
2024-01-14 12:40:04 -0800

    Assessment 3 with a little more progress

---
+++ profile.js
@@ -0,0 +1,29 @@
+console.log('potatooooo');
+
+let color = document.querySelector('#color');
+let place = document.querySelector('#place');
+let ritual = document.querySelector('#ritual');
+
+function pressColor(evt){
+    evt.preventDefault();
+    
+    alert('Blue');
+}
+
+function presPlace(evt){
+    evt.preventDefault();
+    
+    alert('Vietnam');
+}
+
+function pressRitual(evt){
+    evt.preventDefault();
+    
+    alert('Having pho for breakfast!!');
+}
+
+color.addEventListener('click', pressColor);
+
+place.addEventListener('click', presPlace);
+
+ritual.addEventListener('click', pressRitual);
