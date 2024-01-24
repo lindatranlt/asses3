6666ef79bc85e31e1f6a4d0c9e89cd662fe756b7
Linda Tran <133718039+lindatranlt@users.noreply.github.com>
2024-01-23 17:28:48 -0800

    finished assessment 3 part 2

--- profile.js
+++ profile.js
@@ -3,11 +3,12 @@ console.log('potatooooo');
 let color = document.querySelector('#color');
 let place = document.querySelector('#place');
 let ritual = document.querySelector('#ritual');
+let question = document.querySelector('#question');
 
 function pressColor(evt){
     evt.preventDefault();
     
-    alert('Blue');
+    alert('Lilac');
 }
 
 function pressPlace(evt){
@@ -19,7 +20,13 @@ function pressPlace(evt){
 function pressRitual(evt){
     evt.preventDefault();
     
-    alert('Having pho for breakfast!!');
+    alert('Skincare in the morning and night!');
+}
+
+function pressQuestion(evt){
+    evt.preventDefault();
+
+    alert('Wherever you are, I wish you happinessa!')
 }
 
 color.addEventListener('click', pressColor);
@@ -27,3 +34,5 @@ color.addEventListener('click', pressColor);
 place.addEventListener('click', pressPlace);
 
 ritual.addEventListener('click', pressRitual);
+
+question.addEventListener('submit', pressQuestion);
